fa88abb14a20a25afda3970c5f05e5f7295fb448
Davit G <razormayhem4z@gmail.com>
2024-04-07 20:23:34 +1000

    Make jeff distortion multi channel by default

--- dsp/Noise/Jeff.cpp
+++ dsp/Noise/Jeff.cpp
@@ -1,32 +1,35 @@
 #include "Jeff.h"
 
-Jeff::Jeff(juce::AudioProcessorValueTreeState& treeState) : amount(treeState, "jeffAmount") {}
+Jeff::Jeff(juce::AudioProcessorValueTreeState &treeState) : amount(treeState, "jeffAmount") {}
 
 Jeff::~Jeff()
 {
 }
 
-
-void Jeff::prepare(juce::dsp::ProcessSpec& spec) {
+void Jeff::prepare(juce::dsp::ProcessSpec &spec)
+{
 	amount.prepare(spec);
 }
 
-void Jeff::processBlock(juce::dsp::AudioBlock<float>& block) {
+void Jeff::processBlock(juce::dsp::AudioBlock<float> &block)
+{
 	TRACE_EVENT("dsp", "Jeff::processBlock");
 	amount.update();
 
 	auto rightDryData = block.getChannelPointer(1);
 	auto leftDryData = block.getChannelPointer(0);
 
-	for (int sample = 0; sample < block.getNumSamples(); sample++) {
+	for (int sample = 0; sample < block.getNumSamples(); sample++)
+	{
 		float nextJeff = amount.get() * 0.01f;
 
-		auto x = rightDryData[sample];
-		auto a = x * nextJeff;
-		rightDryData[sample] = x + (a * sin(a * 125.6f)) * 0.2f;
+		for (int channel = 0; channel < block.getNumChannels(); channel++)
+		{
+			auto dryData = block.getChannelPointer(channel);
 
-		x = leftDryData[sample];
-		a = x * nextJeff;
-		leftDryData[sample] = x + (a * sin(a * 125.6f)) * 0.2f;
+			auto x = dryData[sample];
+			auto a = x * nextJeff;
+			dryData[sample] = x + (a * sin(a * 125.6f)) * 0.2f;
+		}
 	}
 }
